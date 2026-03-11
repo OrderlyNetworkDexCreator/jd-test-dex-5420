714016fc2a60b29b930608e1e7d303f5a08051dc
Mario Reder <mario.reder@pm.me>
2026-03-11 09:21:53 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Swap,Points",
-  "VITE_CUSTOM_MENUS": "Genusis Airdrop, https://updownfun.win",
+  "VITE_CUSTOM_MENUS": "Genusis Airdrop, https://updownfun.win; AI Monitor, https://www.worldmonitor.app/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
